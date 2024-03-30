77a880e790016195bbcaf75d19041a1b7211e290
1718wani <ikuya1293@gmail.com>
2024-03-30 20:55:59 +0900

    urlを改良

--- app/features/Radioshow/components/RadioShowsCard.tsx
+++ app/features/Radioshow/components/RadioShowsCard.tsx
@@ -1,20 +1,27 @@
 import { Card, Image, Text } from "@mantine/core";
 import { NavLink } from "@remix-run/react";
+import { customeDomain } from "~/consts/customeDomain";
 
 type props = {
-  id:string;
+  id: string;
   imageUrl: string;
   title: string;
 };
 
 export const RadioShowsCard = (props: props) => {
-  const { imageUrl, title , id } = props;
+  const { imageUrl, title, id } = props;
+  const correctImageUrl = `${customeDomain}${imageUrl}`;
+  console.log(correctImageUrl);
 
   return (
     <NavLink to={`/${id}`} style={{ textDecoration: "none" }}>
       <Card shadow="sm" padding="xl" component="a" withBorder>
         <Card.Section>
-          <Image src={imageUrl} fallbackSrc="https://placehold.co/600x400?text=Placeholder" h={160} />
+          <Image
+            src={correctImageUrl}
+            fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+            h={160}
+          />
         </Card.Section>
 
         <Text fw={500} size="lg" mt="md">
