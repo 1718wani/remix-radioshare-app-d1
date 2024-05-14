c23476b3012121c57623ac98cd99e14329a24006
1718wani <ikuya1293@gmail.com>
2024-05-15 00:39:33 +0900

    一旦コメントアウト

--- app/routes/send.ts
+++ app/routes/send.ts
@@ -1,19 +1,19 @@
-import { json } from '@remix-run/node';
-import { Resend } from 'resend';
+// import { json } from '@remix-run/node';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const loader = async () => {
-  const { data, error } = await resend.emails.send({
-    from: 'Acme <onboarding@resend.dev>',
-    to: ['ikuya1293@gmail.com'],
-    subject: 'Hello world',
-    html: '<strong>It works!</strong>',
-  });
+// export const loader = async () => {
+//   const { data, error } = await resend.emails.send({
+//     from: 'Acme <onboarding@resend.dev>',
+//     to: ['ikuya1293@gmail.com'],
+//     subject: 'Hello world',
+//     html: '<strong>It works!</strong>',
+//   });
 
-  if (error) {
-    return json({ error }, 400);
-  }
+//   if (error) {
+//     return json({ error }, 400);
+//   }
 
-  return json(data, 200);
-};
+//   return json(data, 200);
+// };
