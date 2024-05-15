170479a3b0a16c9b90e940344e63b11622127131
1718wani <ikuya1293@gmail.com>
2024-05-15 21:40:49 +0900

    resendが問題なくデプロイできるか確認

--- app/features/Player/hooks/useYoutubePlayer.ts
+++ app/features/Player/hooks/useYoutubePlayer.ts
@@ -1,9 +1,10 @@
 /// <reference types="youtube" />
 
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 
 export function useYouTubePlayer(onStopCallback: () => void) {
   const [player, setPlayer] = useState<YT.Player | null>(null);
+  const hasEndedRef = useRef(false); // フラグを追加
 
   const onPlayerReady = (event: YT.PlayerEvent) => {
     console.log("onPlayerReady", event);
@@ -11,9 +12,13 @@ export function useYouTubePlayer(onStopCallback: () => void) {
 
   const onPlayerStateChange = useCallback(
     (event: YT.OnStateChangeEvent) => {
-      if (event.data === YT.PlayerState.ENDED) {
+      if (event.data === YT.PlayerState.ENDED && !hasEndedRef.current) {
         console.log("onPlayerStateChange2回呼ばれるか", event);
+        hasEndedRef.current = true; // フラグをセット
         onStopCallback();
+        setTimeout(() => {
+          hasEndedRef.current = false; // 少し遅延させてフラグをリセット
+        }, 100); // 100msの遅延を追加
       }
     },
     [onStopCallback]
