d0f57d885405574a5efc802e842bbe39712dca94
1718wani <ikuya1293@gmail.com>
2024-06-17 23:00:52 +0900

    refactor: Spotifyプレイヤーの型エラーと再生時の不具合を修正

--- app/features/Player/types/SpotifyIframeApiTypes.ts
+++ app/features/Player/types/SpotifyIframeApiTypes.ts
@@ -23,22 +23,51 @@ export interface SpotifyIFrameAPI {
 }
 
 export interface SpotifyEmbedController {
-  loadUri(spotifyUri: string ,preferVideo:boolean, timestampInSeconds:number ): void;
-  play(): void;
-  togglePlay(): void;
-  seek(seconds: number): void;
-  destroy(): void;
-  addListener(
-    event: "ready" | "playback_update",
-    callback: (state?: PlaybackState) => void
-  ): void;
+  addListener: (eventName: string, handler: (e: PlaybackState) => void) => void;
+  currentUri: string;
+  destroy: () => void;
+  emitEvent: (eventName: string, eventData: PlaybackState) => void;
+  flushCommandQ: () => void;
+  host: string;
+  iframeElement: HTMLIFrameElement;
+  loadUri: (
+    uriString: string,
+    preferVideo?: boolean,
+    timestampInSeconds?: number
+  ) => void;
+  loading: boolean;
+  off: (eventName: string, handler: (e: PlaybackState) => void) => void;
+  on: (eventName: string, handler: (e: PlaybackState) => void) => void;
+  onPlaybackUpdate: (playbackState: PlaybackState) => void;
+  onPlayerReady: () => void;
+  onWindowMessages: (e: unknown) => void;
+  once: (eventName: string, handler: (e: PlaybackState) => void) => void;
+  options: { uri: string; width: number; height: number };
+  pause: () => void;
+  play: () => void;
+  playFromStart: () => void;
+  playerReadyAck: () => void;
+  queryParamReferer: undefined;
+  removeListener: (eventName: string, handler: (e: PlaybackState) => void) => void;
+  restart: () => void;
+  resume: () => void;
+  seek: (timestampInSeconds: number) => void;
+  sendMessageToEmbed: (messageToSend: unknown) => void;
+  setIframeDimensions: (width: number, height: number) => void;
+  togglePlay: () => void;
+  _commandQ: unknown[];
+  _listeners: {
+    ready: ((e: unknown) => void)[];
+    playback_update: ((e: unknown) => void)[];
+    error: ((e: unknown) => void)[];
+  };
 }
 
 // SpotifyPlayerからのメソッドを含む型を定義
 export interface SpotifyPlayerRef {
   stop: () => void;
-  playEpisode: (uri: string, seekSeconds: number,endSeconds:number) => void;
-  setIframeDimensions: (width: number, height: number) => void;  
+  playEpisode: (uri: string, seekSeconds: number, endSeconds: number) => void;
+  setIframeDimensions: (width: number, height: number) => void;
 }
 
 declare global {
