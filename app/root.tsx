9b5a7cade183175e96e59138a4bfd42301b84261
1718wani <ikuya1293@gmail.com>
2024-06-23 01:33:11 +0900

    refactor: a11yの低下要因となっていたuser-scalable=0を削除

--- app/root.tsx
+++ app/root.tsx
@@ -117,7 +117,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta
           name="viewport"
-          content="width=device-width,initial-scale=1,user-scalable=no"
         />
         <script
           defer
