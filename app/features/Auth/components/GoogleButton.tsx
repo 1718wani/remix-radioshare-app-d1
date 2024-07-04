42acbfce4c833acf5ee456f95d52e38895214d4d
1718wani <ikuya1293@gmail.com>
2024-07-04 20:55:55 +0900

    style: lintエラーの解消

--- app/features/Auth/components/GoogleButton.tsx
+++ app/features/Auth/components/GoogleButton.tsx
@@ -15,6 +15,7 @@ export function GoogleIcon(props: React.ComponentPropsWithoutRef<"svg">) {
 			style={{ width: "0.9rem", height: "0.9rem" }}
 			{...props}
 		>
+			<title>Google Icon</title>
 			<path
 				fill="#4285F4"
 				d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
